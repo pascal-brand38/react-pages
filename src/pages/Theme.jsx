c14e8b0378bf875f366ca91a7011f51be3db0a79
Pascal Brand <pascal.brand38@gmail.com>
2022-12-25 18:23:51 +0100

    Theme: update styles and size in Theme
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -2,51 +2,117 @@
 /// MIT License
 ///
 /// Theme changer (colors,...) from css variables
-/// Note that react's state as useless as changing a css variable value update the
-/// site rendering.
 
+import { useState, useRef } from "react";
+import './Theme.scss';
 
 /// get and set values of css variables
-const cssVar = {
-  get: (name) => {
-    // from https://stackoverflow.com/questions/62750308/getpropertyvalue-includes-whitespace-of-css-formating-when-retrieving-custom-c
-    //    trim() to remove the whitespace in front and end of the css value
-    //    as getPropertyValue reflects how the css is written (with whitespace or without)
-    return getComputedStyle(document.documentElement).getPropertyValue(name).trim();
-  },
-  set: (name, value) => {
-    document.documentElement.style.setProperty(name, value);
-  },
+const cssVar = (name) => {
+  // from https://stackoverflow.com/questions/62750308/getpropertyvalue-includes-whitespace-of-css-formating-when-retrieving-custom-c
+  //    trim() to remove the whitespace in front and end of the css value
+  //    as getPropertyValue reflects how the css is written (with whitespace or without)
+  const init = useRef(getComputedStyle(document.documentElement).getPropertyValue(name).trim());
+
+  const [value, setValue] = useState(init.current);
+
+  const set = (newValue) => {
+    setValue(newValue);
+    document.documentElement.style.setProperty(name, newValue);
+  };
+  const addStep = (step) => {
+    var reValue = /[+-]?\d+(\.\d+)?/g;
+    var reUnit = /[a-zA-Z]+/g;
+    const v = (parseFloat(value.match(reValue)) + step).toFixed(2);
+    const unit = value.match(reUnit);
+    console.log(v.toString() + unit)
+    return v.toString() + unit;
+  }
+
+  const inc = (step) => { set(addStep(step)) }
+  const dec = (step) => { set(addStep(-step)) }
+
+  const reset = () => { set(init.current); }
+
+
+  return { name, value, set, reset, inc, dec };
 }
 
 /// display an input button to choose the color of a css variable
-function ChooseColorInTheme({ nameCssVar }) {
-  const cssVarStyle = {
-    display: "flex",
-    gap: "1rem",
-  }
+function ChooseColorInTheme({ aCssVar }) {
   return (
-    <div style={ cssVarStyle }>
-      <label> { nameCssVar } </label>
-      <input 
-        type = "color"
-        defaultValue = { cssVar.get(nameCssVar)}
-        onChange = { (e) => { cssVar.set(nameCssVar, e.target.value); } }
-        />
+    <div className="theme__item">
+      <label> {aCssVar.name} </label>
+      <label> {aCssVar.value} </label>
+      <input
+        type="color"
+        value={aCssVar.value}
+        onChange={(e) => { aCssVar.set(e.target.value); }}
+      />
+    </div>
+  );
+}
+
+function DecInc({ aCssVar }) {
+  return (
+    <div className="theme__dec-inc">
+      <button className="theme__dec-inc__dec" onClick={() => { aCssVar.dec(0.10); }}> - </button>
+      <div className="theme__dec-inc__text"> {aCssVar.value} </div>
+      <button className="theme__dec-inc__inc" onClick={() => { aCssVar.inc(0.10); }}> + </button>
+    </div>
+  );
+}
+
+/// display an input button to choose the color of a css variable
+function ChooseSizeInTheme({ aCssVar }) {
+  return (
+    <div className="theme__item">
+      <label> {aCssVar.name} </label>
+      <DecInc aCssVar={aCssVar} />
     </div>
   );
 }
 
 
+
 /// Display all css variable we are able to update
 function Theme() {
+  const colors = [
+    cssVar('--body-color'),
+    cssVar('--body-bg-color'),
+    cssVar('--color-1'),
+  ];
+  const sizes = [
+    cssVar('--root-font-size'),
+    cssVar('--margin-1'),
+  ]
+
+  const reset = () => {
+    colors.forEach(color => { color.reset(); });
+    sizes.forEach(size => { size.reset(); });
+  }
+
   return (
     <>
       <h2> Themes</h2>
+      {
+        colors.map((color, index) => {
+          return (
+            <ChooseColorInTheme key={index} aCssVar={color} />
+          );
+        })
+      }
+      {
+        sizes.map((size, index) => {
+          return (
+            <ChooseSizeInTheme key={index} aCssVar={size} />
+          );
+        })
+      }
 
-      <ChooseColorInTheme nameCssVar='--body-color' />
-      <ChooseColorInTheme nameCssVar='--body-bg-color' />
-      <ChooseColorInTheme nameCssVar='--color-1' />
+      <button className="pbr-button"
+        onClick={reset}>
+        Reset
+      </button>
 
     </>
   );
